19bd3729ef9952aa0f1d8a0dbf19963421352c47
post <post@byggkompaniet.no>
2026-02-19 20:31:06 +0000

    Manual code change

--- src/components/admin/CreateUserDialog.jsx
+++ src/components/admin/CreateUserDialog.jsx
@@ -293,7 +293,7 @@ KS System Team
           </Button>
           <Button
             onClick={handleSubmit}
-            disabled={createUserMutation.isPending} className="bg-green-600 text-primary-foreground px-4 py-2 text-sm font-medium rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow hover:bg-primary/90 h-9">
+            disabled={createUserMutation.isPending} className="bg-green-700 text-primary-foreground px-4 py-2 text-sm font-medium rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow hover:bg-primary/90 h-9">
 
             {createUserMutation.isPending ?
             <>
