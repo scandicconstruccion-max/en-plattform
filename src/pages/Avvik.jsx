a0b6e68453e7325157cbd053db5c595582f8801c
post <post@byggkompaniet.no>
2026-02-21 10:37:09 +0000

    Manual code change

--- src/pages/Avvik.jsx
+++ src/pages/Avvik.jsx
@@ -1208,7 +1208,7 @@ export default function Avvik() {
               onClick={() => {
                 setShowStatusDialog(false);
                 setNewStatus('');
-              }} className="bg-background px-4 py-10 text-sm font-medium rounded-xl inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input shadow-sm hover:bg-accent hover:text-accent-foreground h-9">
+              }} className="bg-background px-4 py-1 text-sm font-medium rounded-xl inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input shadow-sm hover:bg-accent hover:text-accent-foreground h-9">
 
 
               Avbryt
