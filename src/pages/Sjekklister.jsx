c39917d9c30c262512548ad27a7bf80ac7adbcc7
post <post@byggkompaniet.no>
2026-02-22 20:22:04 +0000

    File changes

--- src/pages/Sjekklister.jsx
+++ src/pages/Sjekklister.jsx
@@ -115,7 +115,7 @@ export default function Sjekklister() {
                 Ny mal
               </Button> :
 
-          <Button onClick={() => setShowNewChecklistDialog(true)} className="bg-green-700 text-primary-foreground px-4 py-2 text-sm font-medium rounded-md inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow hover:bg-primary/90 h-9 gap-2">
+          <Button onClick={() => setShowNewChecklistDialog(true)} className="gap-2">
                 <Plus className="h-4 w-4" />
                 Ny sjekkliste
               </Button>
