8dae5087e88710b26bf66aabc4597679a6f93fa1
post <post@byggkompaniet.no>
2026-02-24 14:21:19 +0000

    File changes

--- src/components/ressursplan/OptimizedResourceCalendar.jsx
+++ src/components/ressursplan/OptimizedResourceCalendar.jsx
@@ -662,7 +662,7 @@ export default function OptimizedResourceCalendar({
           </div>
 
           {/* Scrollable Calendar Body */}
-          <div className="flex-1 overflow-x-auto overflow-y-hidden relative">
+          <div className="flex-1 overflow-hidden relative">
             {/* Today Marker - Vertical Line */}
             {viewDates.map((day, index) => {
               const isToday = isSameDay(day, new Date());
