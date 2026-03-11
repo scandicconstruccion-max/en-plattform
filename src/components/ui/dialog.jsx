5a14bb51fc4b08c9433651a676f20c7edd3b8fba
post <post@byggkompaniet.no>
2026-03-11 12:13:16 +0000

    File changes

--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -35,9 +35,7 @@ const DialogContent = React.forwardRef(({ className, children, ...props }, ref) 
         className
       )}
       {...props}>
-      <div className="flex-1 overflow-y-auto overflow-x-hidden p-6">
         {children}
-      </div>
       <DialogPrimitive.Close
         className="absolute right-4 top-4 z-10 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground bg-background">
         <X className="h-4 w-4" />
